608e05ec3d31feb2cea48b4db0d9e672860e4bd2
Georgia <georgia@georgia>
2015-11-13 20:04:23 -0500

    made the servo position change

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/QuantumMechanics/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/QuantumMechanics/Autonomous.java
@@ -15,11 +15,11 @@ public class Autonomous extends LinearOpMode {
 
     DcMotor mL1;
     DcMotor mL2;
-    DcMotor mL3;
+ //   DcMotor mL3;
 
     DcMotor mR1;
     DcMotor mR2;
-    DcMotor mR3;
+  //  DcMotor mR3;
 
 
     int initPos;
@@ -33,7 +33,7 @@ public class Autonomous extends LinearOpMode {
         ticks = Math.abs(ticks);
 
         // Get pos
-        initPos = mL1.getCurrentPosition();
+        initPos = mR2.getCurrentPosition();
         // tFR = mFR.getCurrentPosition();
 
         drive(power); // Start driving
@@ -62,7 +62,7 @@ public class Autonomous extends LinearOpMode {
         ticks = Math.abs(ticks);
 
         // Get pos
-        initPos = mL1.getCurrentPosition();
+        initPos = mR2.getCurrentPosition();
 
         drive(power, -power);
 
@@ -83,19 +83,19 @@ public class Autonomous extends LinearOpMode {
         mL1.setPower(lPower);
         //    mL2.setPower(lPower);
         mL2.setPower(lPower);
-        mL3.setPower(rPower);
+     //   mL3.setPower(rPower);
 
         mR1.setPower(rPower);
         //   mR2.setPower(rPower);
         mR2.setPower(rPower);
-        mR3.setPower(rPower);
+    //    mR3.setPower(rPower);
 
     }
 
     private int driveEncoders () {
 
 
-        return Math.abs(mL1.getCurrentPosition() - initPos) ;
+        return Math.abs(mR2.getCurrentPosition() - initPos) ;
     }
 
 
@@ -105,63 +105,46 @@ public class Autonomous extends LinearOpMode {
         mL1 = hardwareMap.dcMotor.get("mL1");
         //   mL2 = hardwareMap.dcMotor.get("mL2");
         mL2 = hardwareMap.dcMotor.get("mL2");
-        mL3 = hardwareMap.dcMotor.get("mL3");
+     //   mL3 = hardwareMap.dcMotor.get("mL3");
 
         mR1 = hardwareMap.dcMotor.get("mR1");
         // mR2 = hardwareMap.dcMotor.get("mR2");
         mR2 = hardwareMap.dcMotor.get("mR2");
-        mR3 = hardwareMap.dcMotor.get("mR3");
+      //  mR3 = hardwareMap.dcMotor.get("mR3");
 
 
         mR1.setDirection(DcMotor.Direction.REVERSE);
         //  mR2.setDirection(DcMotor.Direction.REVERSE);
-        mR2.setDirection(DcMotor.Direction.REVERSE);
-        mR3.setDirection(DcMotor.Direction.REVERSE);
+        mL1.setDirection(DcMotor.Direction.REVERSE);
+        mL2.setDirection(DcMotor.Direction.REVERSE);
 
 
         waitForStart();
 //get to bucket
-        //go up
+        driveTicks(-.5, 200);
+        sleep(200);
 
+
+   //     turnTicks(.5, 1600);
+    //    sleep(200);
+
+
+   //     driveTicks(-.5, 2000);
+  //      sleep(200);
+
+
+
+        //go up
+    pinion.setPosition(.35);
+        sleep(1000);
+        pinion.setPosition(1);
+        sleep(1000);
 
 //dumbs climbers
         climbers.setPosition(.1);
         sleep(1000);
         climbers.setPosition(1);
         sleep(1000);
-
-
-        driveTicks(.5, -2000);
-        sleep(200);
-
-
-        turnTicks(.5, 1600);
-        sleep(200);
-
-
-        driveTicks(.5, -2000);
-        sleep(200);
-
-
-        turnTicks(.5, 1600);
-        sleep(200);
-
-
-        driveTicks(.8, -2000);
-        sleep(200);
-
-        driveTicks(.8, 1000);
-        sleep(200);
-        driveTicks(.8, -1000);
-        sleep(200);
-        driveTicks(.5, 5000);
-        sleep(200);
-
-        turnTicks(.5, 800);
-        sleep(200);
-
-
-        driveTicks(1, - 5000);
 
     }
 }
